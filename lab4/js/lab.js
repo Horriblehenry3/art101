82f9bdef051405267851cefbebd0855e99945024
Horriblehenry3 <91556516+Horriblehenry3@users.noreply.github.com>
2021-10-18 21:56:28 -0700

    finished lab5

---
+++ lab4/js/lab.js
@@ -0,0 +1,13 @@
+// Summary. (LAN based messaging/ file transfer system)
+// Description. (taking)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  10.12.21
+
+//Create a LAN based messaging/private file share system
+    //Step one: Connect to a network
+    //Step two: Ping network for other devices on network to find discoverable I.P’s
+    //Step three: Request a connection to other machines on the network via message via command prompt.
+    //Step four: Establish connection between two I.P’s
+    //Step five: Send packet of data from machine directly to other machine using only shared network.
---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Henry Merry
+ * Created:   10/18/21
+ *
+ * (c) Copyright by me
+ **/
+
+ //define variables
+ make = "Ford";
+ model = "Taurus";
+ color =  "Rusty";
+ year = 1995;
+
+ //calculate
+ age = 2021 - year;
+
+ //output document.writein("make:" + make + "<br>" );
+document.writeln("model:" + model + "<br>" );
+document.writeln("color:" + color + "<br>" );
+document.writeln("year:" + year + "<br>" );
+document.writeln("age:" + age + " years<br>" );
+document.writeln("make:" + make + "<br>" );
